fa7bd3247302407da423c38690b07b0917fadb80
Ivar Troost <ivar@desagaz.com>
2017-06-13 15:45:42 +0200

    Add a GeoJSON book locations hook
    Located at URL/api/bookLocs.geojson.
    Returns all book locations.

--- core/urls.py
+++ core/urls.py
@@ -6,3 +6,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
+
+# Use GeoJSON api
+from djgeojson.views import GeoJSONLayerView
+from .models import BookLocation
+
+# GeoJSON book locations hook. Returns object with all BookLocation elements.
+urlpatterns += [
+	url(r'^api/bookLocs.geojson$', GeoJSONLayerView.as_view(model=BookLocation, properties=('name','book_holding','time',)), name='bookLocs'),
+]
