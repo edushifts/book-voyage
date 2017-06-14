e5b5e149f9e5ba6692cd0e737a6d737c66f47318
Ivar Troost <ivar@desagaz.com>
2017-06-14 23:55:16 +0200

    Improve map rendering
    Map is bounded to world now, has zoomed out initial view and displays pop-ups with messages.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,38 @@
+// Define maximum map bounds (to avoid moving off the map)
+bounds = new L.LatLngBounds(new L.LatLng(85, -180), new L.LatLng(-85, 180));
+
 // Initate Leaflet map, including initial location and scale
-var mainMap = L.map('mainMap').setView([51.505, -0.09], 13);
+var mainMap = L.map('mainMap', {
+	worldCopyJump: true,
+	maxBounds: bounds,
+	maxBoundsViscosity: 1.0
+}).setView([51.505, -0.09], 3);
 
 // Connect to the map tile provider and add tiles to the map
 var Stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
-	minZoom: 1,
+	minZoom: 3,
 	maxZoom: 16,
-	ext: 'png'
+	ext: 'png',
 }).addTo(mainMap);
 
-// TEMPORARY - to be handled with Angular
-// Add all book locations to the map without discrimination
-var layer = L.geoJson();
-mainMap.addLayer(layer);
-$.getJSON(location.origin + "/api/bookLocs.geojson", function (data) {
-    layer.addData(data);
-    //console.log(bookLocations.features[1].id);
-});
+// Defines what happens when each location is loaded
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.message) {
+        layer.bindPopup("<b>NAME PERSON</b><br>" + feature.properties.message);
+    }
+}
+
+// Loads geojson file, displays locations on map and
+// performs onEachFeature function on each
+$.ajax({
+dataType: "json",
+url: location.origin + "/api/bookLocs.geojson",
+success: function(data) {
+	L.geoJSON(data, {
+	    onEachFeature: onEachFeature
+	}).addTo(mainMap);
+}
+}).error(function() {});
