ac905d375b7570178daefd530c1845dbb2b5f247
Ivar Troost <ivar@desagaz.com>
2017-07-08 15:34:07 +0200

    Implement first version new api (breaks app)

--- bookvoyage-frontend/src/app/overview-map/overview-map.component.ts
+++ bookvoyage-frontend/src/app/overview-map/overview-map.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {ActivatedRoute} from "@angular/router";
 import {HeaderService} from "../header/header.service";
+import { Observable } from 'rxjs';
+import {Http, Response} from "@angular/http";
 
 // prevents Typescript errors with leaflet and jquery
 declare let L: any;
@@ -54,7 +56,38 @@ export class OverviewMapComponent implements OnInit {
   wrongCode = false;
 
   constructor(private route: ActivatedRoute,
-              private headerService: HeaderService) { }
+              private headerService: HeaderService,
+              private http: Http) { }
+
+  getBookInstances() {
+    return this.http.get(environment.apiUrl + "api/bookInstances/")
+      .map(
+        (response: Response) => {
+          // on success, return token
+          // console.log(response.json().token);
+          return response.json();
+        }
+      )
+      .catch(
+        (error: Response) => {
+          return Observable.throw(error);
+        });
+  }
+
+  getBookInstance(id: number) {
+    return this.http.get(environment.apiUrl + "api/bookInstances/")
+      .map(
+        (response: Response) => {
+          // on success, return token
+          // console.log(response.json().token);
+          return response.json()[id];
+        }
+      )
+      .catch(
+        (error: Response) => {
+          return Observable.throw(error);
+        });
+  }
 
   ngOnInit() {
     this.headerService.showAccountButtons = true;
@@ -96,16 +129,17 @@ export class OverviewMapComponent implements OnInit {
       ext: 'png',
     }).addTo(mainMap);
 
+    console.log("Still working");
+
     // Loads geojson file, displays locations on map and
     // performs onEachFeature function on each
-    $.ajax({
-      dataType: "json",
-      url: environment.apiUrl + "api/bookLocs.geojson",
-      success: function(data) {
+    this.getBookInstances().subscribe(
+      (bookInstances) => {
 
+        console.log("Been there");
         // look at each book location provided by the api
-        $.each(data.features, function(i, obj) {
-
+        for (let bookInstance of bookInstances) {
+          console.log("and there");
           // create new class for the current book
           let geoData = new GeoData();
 
@@ -121,45 +155,50 @@ export class OverviewMapComponent implements OnInit {
             //popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
           });
 
-          var currentMarker = new L.marker([obj.geometry.coordinates[1], obj.geometry.coordinates[0]], {icon: blueIcon});
+          // add special marker for batch location
+          // TODO: make batch locations be rendered only once
+
+          let batch = bookInstance.batch;
+          let batchLocation = bookInstance.batch.location;
+
+          var currentMarker = new L.marker([batchLocation.coordinates[1], batchLocation.coordinates[0]], {icon: blueIcon});
 
           // add message if available
-          if (obj.properties.message) {
-            currentMarker.bindPopup("<b>NAME PERSON</b><br>" + obj.properties.message + "<br>" + obj.properties.time );
+          if (batchLocation) {
+            currentMarker.bindPopup("<b>Event:" + batch.event + "</b><br>" + batch.country + "<br>" + batch.date );
           }
 
           geoData.marker = currentMarker;
-          geoData.holder = obj.properties.holder;
-          geoData.book_instance = obj.properties.book_instance;
+          // geoData.holder = obj.properties.holder;
+          // geoData.book_instance = obj.properties.book_instance;
 
-          var newLayer = mainMap.addLayer(geoData.marker);
+          var newLayer = mainMap.addLayer(currentMarker);
 
           // check if previous holding was of same book
           // if so, draw line between them
-          if (i != 0 && geoDataArray[i].book_instance === geoDataArray[i-1].book_instance) {
-            var latlngs = Array();
-            latlngs.push(geoDataArray[i-1].marker.getLatLng());
-            latlngs.push(geoDataArray[i].marker.getLatLng());
-            var color = rainbow(geoDataArray[i].book_instance, 10);
-
-            var polyline = L.polyline(latlngs, {color: color}).addTo(mainMap);
-
-            // TODO: FUNCTION CURRENTLY NOT WORKING
-            // var arrowHead = L.polylineDecorator(polyline, {
-            //   patterns: [
-            //     {
-            //       offset: '50%',
-            //       repeat: 0,
-            //       symbol: L.Symbol.arrowHead({pixelSize: 15, polygon: false, pathOptions: {stroke: true, color: color}})
-            //     }
-            //   ]
-            // }).addTo(mainMap);
-          }
-        });
+          // if (i != 0 && geoDataArray[i].book_instance === geoDataArray[i-1].book_instance) {
+          //   var latlngs = Array();
+          //   latlngs.push(geoDataArray[i-1].marker.getLatLng());
+          //   latlngs.push(geoDataArray[i].marker.getLatLng());
+          //   var color = rainbow(geoDataArray[i].book_instance, 10);
+          //
+          //   var polyline = L.polyline(latlngs, {color: color}).addTo(mainMap);
+          //
+          //   // TODO: FUNCTION CURRENTLY NOT WORKING
+          //   // var arrowHead = L.polylineDecorator(polyline, {
+          //   //   patterns: [
+          //   //     {
+          //   //       offset: '50%',
+          //   //       repeat: 0,
+          //   //       symbol: L.Symbol.arrowHead({pixelSize: 15, polygon: false, pathOptions: {stroke: true, color: color}})
+          //   //     }
+          //   //   ]
+          //   // }).addTo(mainMap);
+          // }
+        }
       },
-      error: function() {
-        console.log("Error loading map :(");
-      }
-    });
+      (errorData) => {
+        console.log("Error loading book locations: " + errorData);
+      });
   }
 }
