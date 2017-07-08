1b1a0a22fe3a5cf7954a338bd626a00038dfb0d6
Ivar Troost <ivar@desagaz.com>
2017-07-08 15:32:08 +0200

    Remove deprecated geojson hook

--- bookvoyage-backend/core/urls.py
+++ bookvoyage-backend/core/urls.py
@@ -1,18 +1,11 @@
-from django.conf.urls import url
-from . import views
 from django.conf.urls import url, include
 from core import views
 from rest_framework.routers import DefaultRouter
 from django.views.generic.base import TemplateView
 
-# Use GeoJSON api
-from djgeojson.views import GeoJSONLayerView
-from .models import BookHolding
-
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'bookInstances', views.BookInstanceViewSet)
-router.register(r'bookHoldings', views.BookHoldingViewSet)
 
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
@@ -24,17 +17,23 @@ urlpatterns = [
     url(r'^api-auth/registration/', include('rest_auth.registration.urls')),
 ]
 
+
+# Use GeoJSON api
+# from djgeojson.views import GeoJSONLayerView
+# from .models import BookHolding
+
 # GeoJSON book locations hook. Returns object with all BookLocation elements.
-urlpatterns += [
-    url(r'^api/bookLocs.geojson$',
-        GeoJSONLayerView.as_view(
-            model=BookHolding,
-            geometry_field='location',
-            properties=('book_instance','holder','time','message',
-                        )
-        ),
-        name='bookLocs',),
-]
+# Deprecated, now included in book registration
+# urlpatterns += [
+#     url(r'^api/bookLocs.geojson$',
+#         GeoJSONLayerView.as_view(
+#             model=BookHolding,
+#             geometry_field='location',
+#             properties=('book_instance','holder','time','message',
+#                         )
+#         ),
+#         name='bookLocs',),
+# ]
 
 # catch-all for Angular app
 urlpatterns += [
