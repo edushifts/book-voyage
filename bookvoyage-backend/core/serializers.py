8547e5aa28e25c9551a1ba79e68a14a62d7d5550
Ivar Troost <ivar@desagaz.com>
2017-07-11 12:31:25 +0200

    Add anonimity option for users

--- bookvoyage-backend/core/serializers.py
+++ bookvoyage-backend/core/serializers.py
@@ -4,6 +4,21 @@ from core.models import BookInstance, BookHolding, BookBatch, BookOwning
 from django.contrib.auth.models import User
 
 class UserGenSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+
+    def get_first_name(self, user):
+        if user.groups.filter(name='Anonymous').exists():
+            return "Anonymous"
+        else:
+            return user.first_name
+
+    def get_last_name(self, user):
+        if user.groups.filter(name='Anonymous').exists():
+            return ""
+        else:
+            return user.last_name
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name')
