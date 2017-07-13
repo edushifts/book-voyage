a75ca43b3035f3f391b39393802ea46d440b22c5
Ivar Troost <ivar@desagaz.com>
2017-07-14 01:19:26 +0200

    Add option to bulk-add users
    Warning: excel import is shaky with importing groups; json import is recommended.

--- bookvoyage-backend/core/admin.py
+++ bookvoyage-backend/core/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+# Register models
 from .models import Author, Book, BookInstance, BookHolding, BookOwning, BookBatch
+from django.contrib.auth.models import User
 
 class BookResource(resources.ModelResource):
     class Meta:
@@ -23,6 +24,17 @@ class BookCodeResource(resources.ModelResource):
 class BookCodeAdmin(ImportExportModelAdmin):
     resource_class = BookCodeResource
 
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'groups', 'username')
+        import_id_fields = ['username']
+        skip_unchanged = True
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+    pass
+
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookCodeAdmin)
@@ -30,3 +42,5 @@ admin.site.register(BookHolding, LeafletGeoAdmin)
 admin.site.register(BookOwning, LeafletGeoAdmin)
 admin.site.register(BookBatch, LeafletGeoAdmin)
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
