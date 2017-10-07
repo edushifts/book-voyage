577f79072ac39f0a12dfc373b92cbc778af99624
Ivar Troost <ivar@desagaz.com>
2017-10-08 00:19:38 +0200

    Set language to browser language by default

--- bookvoyage-frontend/src/app/app.component.ts
+++ bookvoyage-frontend/src/app/app.component.ts
@@ -15,5 +15,6 @@ export class AppComponent {
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     // translate.use('nl'); // DEBUG
+    translate.use(translate.getBrowserLang());
   }
 }
