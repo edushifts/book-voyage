5b9fdfb8c1fb3c80929ffbde409164d908f9c26c
Ivar Troost <ivar@desagaz.com>
2017-10-10 01:01:02 +0200

    Fix fixed language bug

--- bookvoyage-frontend/src/app/app.component.ts
+++ bookvoyage-frontend/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     translate.setDefaultLang('en');
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use('es'); // DEBUG
-    // translate.use(translate.getBrowserLang());
+    // translate.use('es'); // DEBUG
+    translate.use(translate.getBrowserLang());
   }
 }
